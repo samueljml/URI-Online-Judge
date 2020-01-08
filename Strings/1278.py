7a16e49b79fb5d580f67b76addf137230be137cf
Samuel <54119245+samueljml@users.noreply.github.com>
2020-01-08 13:24:23 -0300

    1278 - Justificador II

--- Strings/1278.py
+++ Strings/1278.py
@@ -1,1 +1,29 @@
+idf = False
 
+while True:
+    
+    qtd = int(input())
+    maior = 0
+    if qtd == 0:
+        break
+    if idf:
+        print()
+    texto = []
+
+    for i in range(qtd):
+        line = input().split()
+        linha = ''
+        tam = len(line)
+        for j in range(tam):     
+            if j+1 == tam:
+                linha += line[j]
+            else:
+                linha += line[j]
+                linha += ' '
+        tam2 = len(linha)
+        if tam2 > maior:
+            maior = tam2
+        texto.append(linha)
+    for i in range(qtd):
+        print(texto[i].rjust(maior))
+    idf = True
