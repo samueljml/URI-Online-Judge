33bf4ee0342abed897e1476bf72cffe915f01708
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-03-18 20:09:24 -0300

    URI 1366.java

---
+++ AD-HOC/1366/1366.java
@@ -0,0 +1,29 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while (true) {
+			int qtdVaretas = scan.nextInt();
+			int total = 0;
+			int metades = 0;
+
+			if (qtdVaretas == 0)
+				break;
+
+			while(qtdVaretas-- > 0) {
+				scan.nextInt();
+				int quantidade = scan.nextInt();
+
+				total += quantidade / 4;
+				quantidade %= 4;
+
+				metades += quantidade / 2;
+			}
+			System.out.println((total + metades/2));
+		}
+		scan.close();
+	}
+}
