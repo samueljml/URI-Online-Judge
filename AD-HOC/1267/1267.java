f5aa5a7328a49744b880113a234d64b0088f5189
Samuel <samuelkllv@gmail.com>
2021-03-18 17:27:15 -0300

    URI 1267.java

---
+++ AD-HOC/1267/1267.java
@@ -0,0 +1,34 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while (true) {
+			int numeroAlumni = scan.nextInt();
+			int numeroJantares = scan.nextInt();
+			String saida = "no";
+
+			if (numeroAlumni == 0 && numeroJantares == 0)
+				break;
+
+			int[] alunos = new int[numeroAlumni];
+
+			for (int jantares = 0; jantares < numeroJantares; jantares++) {
+				for (int i = 0; i < numeroAlumni; i++) {
+					alunos[i] += scan.nextInt();
+				}
+			}
+
+			for (int i = 0; i < numeroAlumni; i++) {
+				if (alunos[i] == numeroJantares) {
+					saida = "yes";
+				}
+			}
+
+			System.out.println(saida);
+		}
+		scan.close();
+	}
+}
